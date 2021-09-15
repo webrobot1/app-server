726725a93e4171371f363f43d06d122cf0176b44
Стрельцов Михаил <admin@my-fantasy.ru>
2021-09-15 20:49:04 +0300

    no message

--- model/ServerModel.php
+++ model/ServerModel.php
@@ -65,130 +65,138 @@ class ServerModel extends \Edisom\Core\Model
 		// а то все идет по пизде с проверко наличие фаилов Workerman в папке /tmp
 		// systemctl daemon-reload  и перезапускаем apache 
 		
-		\Workerman\Worker::$logFile = static::temp().'main.log';
+		if(static::config('protocol') && ($address = strtolower(static::config('protocol')).'//0.0.0.0:'.static::config('port')))
+		{
+			\Workerman\Worker::$logFile = static::temp().'main.log';
 
-		$this->socket = new \Workerman\Worker('websocket://0.0.0.0:'.static::config('port'));
-		$this->socket->onWorkerStart = function($worker)
-		{	
+			static::log('открываем сервер на '.$address);	
+			$this->socket = new \Workerman\Worker('websocket://0.0.0.0:'.static::config('port'));
+			
 			// персональный протокол (для decode и encode сообщений)
 			// todo понадогбиться разделять Json пакеты друг от друга (придумать разделитель, типа \n)
-			if(static::config('protocol'))
-				$worker->protocol = static::config('protocol');
+			$this->socket->protocol = "\\Edisom\\App\\server\\model\\Protocols\\".static::config('protocol');
 			
-			//@unlink(static::temp().'main.log');
-			//@unlink(static::temp().'error.log');
-			
-			static::log("очищаем Redis");
-			static::redis()->flushAll();
-						
-			
-			// если что то придет из других приложений (из Redis) - сообщим всем на карте
-			// todo пока тут одна карта но нужен массив из всех карт
-			$subscribe = new \Workerman\Redis\Client('redis://127.0.0.1:6379');
-			$subscribe->subscribe(['map:1'], function ($channel, $message) 
-			{
-				foreach(static::redis()->zRange($channel, 0, -1) as $token)
-				{	
-					// если у нас есть соединение  
-					if(isset($this->socket->connections[$this->tokens[$token]]))
-					{	
-						static::log('Шлем '.$token.': '.$message);					
-						$this->socket->connections[$this->tokens[$token]]->send($message);
-					}
-					else
-					{
-						// если нет - удалим из редиса данные токена
-						static::log('Пользователь '.$token.' не найден');	
-						$this->remove($token);
-					}
-				}
-			});
-			
-			static::log("запускаем таймер");	
-			
-			// сохраняем всех игроков раз в период
-			\Workerman\Lib\Timer::add(static::$pingPause, function()
-			{
-				foreach ($this->tokens as $token=>$id) 
-				{	
-					if(($time = strtotime(static::redis()->hGet($token, 'datetime'))) && strtotime("+5 minute", $time) < time())
-					{
-						$this->disconect($token, 'Таймаут');
-					}
-					else
-					{
-						if($time >= time()-static::$pingPause){
-							static::log("запрос скриншета");	
-							$this->socket->connections[$id]->send(json_encode(['action'=>'screen']));
-						}
-						
-						$this->save($token);
-					}
-				}			
-			}); 
-		}; 
+			$this->socket->onWorkerStart = function($worker)
+			{	
+				static::log('Используемый протокол: '.$worker->protocol);
 				
-		$this->socket->onWorkerStop = function($worker)
-		{	
-			foreach (array_keys($this->tokens) as $token) 
-				$this->disconect($token, 'сервер остановлен');		
-		};	
-		
-		$this->socket->onClose = function($connection)
-		{
-			if($token = array_search($connection->id, $this->tokens))
-			{
+				//@unlink(static::temp().'main.log');
+				//@unlink(static::temp().'error.log');
 				
-				// обнулим параметры (они обнулятся при сохранении игрока в БД)
-				static::redis()->hSet($token, 'ip', '');
-				static::redis()->hSet($token, 'token', '');
-				static::redis()->hSet($token, 'action', 'offline');
+				static::log("очищаем Redis");
+				static::redis()->flushAll();
+							
 				
-				
-				$this->save($token);
-				$this->remove($token);
-			}
-		};
-		
-		$this->socket->onError = function($connection, $code, $msg)
-		{
-			$connection->close("error $code $msg");
-		};
-		
-		$this->socket->onMessage = function($connection, array $data)
-		{ 
-			static::log('клиент сказал '.print_r($data, true));
-		
-			// токен передаем только в первом сообщении (дальше его из переменной $this->tokens берем по установленному соединению)
-			if((isset($data['token']) || ($data['token'] = array_search($connection->id, $this->tokens))) && static::redis()->hExists($data['token'], 'id'))
-			{								
-				// запишем кто сидит из под токеном что бы слать ответ
-				if(!array_key_exists($data['token'], $this->tokens))
+				// если что то придет из других приложений (из Redis) - сообщим всем на карте
+				// todo пока тут одна карта но нужен массив из всех карт
+				$subscribe = new \Workerman\Redis\Client('redis://127.0.0.1:6379');
+				$subscribe->subscribe(['map:1'], function ($channel, $message) 
 				{
-					$this->tokens[$data['token']] = $connection->id;
-					static::redis()->hSet($data['token'], 'ip' , $connection->getRemoteAddress());
-				}
-					
-				// обновим в редисе данные статические	
-				static::redis()->hSet($data['token'], 'datetime' , date("Y-m-d H:i:s"));
-				if(isset($data['pingTime']))
-					static::redis()->hSet($data['token'], 'ping' , $data['pingTime']);
-					
-				// можно еще в $data['action'] слать первым парметром приложение (ну пока только game используем)
-				// можно переделать на HTTP (типа Rabbit) , тогда вызываем метод по адресной строке вида (последняя часть - GET параметры распарсенные из массива):
-				// /game/$controller/$action/?static::explode($data, '&', false) 
-				list($controller, $action) = array_replace_recursive(array('api', 'index'), array_filter(explode('/', $data['action'])));
-					
-				static::redis()->hSet($data['token'], 'action', $data['action']);	
-				unset($data['action']);
+					foreach(static::redis()->zRange($channel, 0, -1) as $token)
+					{	
+						// если у нас есть соединение  
+						if(isset($this->socket->connections[$this->tokens[$token]]))
+						{	
+							static::log('Шлем '.$token.': '.$message);					
+							$this->socket->connections[$this->tokens[$token]]->send($message);
+						}
+						else
+						{
+							// если нет - удалим из редиса данные токена
+							static::log('Пользователь '.$token.' не найден');	
+							$this->remove($token);
+						}
+					}
+				});
 				
-				$this->run($controller, $action, $data);					
-			}
-			else{
-				$connection->close('токен не найден');
-			}
-		};
+				static::log("запускаем таймер");	
+				
+				// сохраняем всех игроков раз в период
+				\Workerman\Lib\Timer::add(static::$pingPause, function()
+				{
+					foreach ($this->tokens as $token=>$id) 
+					{	
+						if(($time = strtotime(static::redis()->hGet($token, 'datetime'))) && strtotime("+5 minute", $time) < time())
+						{
+							$this->disconect($token, 'Таймаут');
+						}
+						else
+						{
+							if($time >= time()-static::$pingPause){
+								static::log("запрос скриншета");	
+								$this->socket->connections[$id]->send(json_encode(['action'=>'screen']));
+							}
+							
+							$this->save($token);
+						}
+					}			
+				}); 
+			}; 
+					
+			$this->socket->onWorkerStop = function($worker)
+			{	
+				foreach (array_keys($this->tokens) as $token) 
+					$this->disconect($token, 'сервер остановлен');		
+			};	
+			
+			$this->socket->onClose = function($connection)
+			{
+				if($token = array_search($connection->id, $this->tokens))
+				{
+					
+					// обнулим параметры (они обнулятся при сохранении игрока в БД)
+					static::redis()->hSet($token, 'ip', '');
+					static::redis()->hSet($token, 'token', '');
+					static::redis()->hSet($token, 'action', 'offline');
+					
+					
+					$this->save($token);
+					$this->remove($token);
+				}
+			};
+			
+			$this->socket->onError = function($connection, $code, $msg)
+			{
+				$connection->close("error $code $msg");
+			};
+			
+			$this->socket->onMessage = function($connection, array $data)
+			{ 
+				static::log('клиент сказал '.print_r($data, true));
+			
+				// токен передаем только в первом сообщении (дальше его из переменной $this->tokens берем по установленному соединению)
+				if((isset($data['token']) || ($data['token'] = array_search($connection->id, $this->tokens))) && static::redis()->hExists($data['token'], 'id'))
+				{								
+					// запишем кто сидит из под токеном что бы слать ответ
+					if(!array_key_exists($data['token'], $this->tokens))
+					{
+						$this->tokens[$data['token']] = $connection->id;
+						static::redis()->hSet($data['token'], 'ip' , $connection->getRemoteAddress());
+					}
+						
+					// обновим в редисе данные статические	
+					static::redis()->hSet($data['token'], 'datetime' , date("Y-m-d H:i:s"));
+					if(isset($data['pingTime']))
+						static::redis()->hSet($data['token'], 'ping' , $data['pingTime']);
+						
+					// можно еще в $data['action'] слать первым парметром приложение (ну пока только game используем)
+					// можно переделать на HTTP (типа Rabbit) , тогда вызываем метод по адресной строке вида (последняя часть - GET параметры распарсенные из массива):
+					// /game/$controller/$action/?static::explode($data, '&', false) 
+					list($controller, $action) = array_replace_recursive(array('api', 'index'), array_filter(explode('/', $data['action'])));
+						
+					static::redis()->hSet($data['token'], 'action', $data['action']);	
+					unset($data['action']);
+					
+					$this->run($controller, $action, $data);					
+				}
+				else{
+					$connection->close('токен не найден');
+				}
+			};
 
-		\Workerman\Worker::runAll();
+			\Workerman\Worker::runAll();
+		}
+		else
+			throw new \Exception('не определен протокол');
 	}	
 }
