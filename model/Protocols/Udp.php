128cebf10fb444399a7e21c1154583c7cc9035b6
Стрельцов Михаил <admin@my-fantasy.ru>
2021-09-16 16:36:10 +0300

    no message

--- model/Protocols/Udp.php
+++ model/Protocols/Udp.php
@@ -3,5 +3,9 @@ namespace Edisom\App\server\model\Protocols;
 
 class Udp extends Tcp
 {
-      
+    public static function encode($buffer):string
+    {
+		Worker::log("Шлем: ".$buffer);
+		return $buffer;
+    }    
 }
