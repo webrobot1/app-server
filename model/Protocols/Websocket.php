e6e7194ac9ca38e6278a45322822f9a29cdcd295
Стрельцов Михаил <admin@my-fantasy.ru>
2021-09-16 19:49:40 +0300

    no message

--- model/Protocols/Websocket.php
+++ model/Protocols/Websocket.php
@@ -6,13 +6,19 @@ class Websocket extends \Workerman\Protocols\Websocket
 {	
 	public static function encode($buffer, \Workerman\Connection\ConnectionInterface $connection)
     {
-		Worker::log("Шлем: ".$buffer);
-		return parent::encode($buffer, $connection);
+		if($buffer = parent::encode($buffer, $connection))
+		{
+			Worker::log("Шлем: ".$buffer);
+			return $buffer;
+		}
 	}
 	
     public static function decode($buffer, \Workerman\Connection\ConnectionInterface $connection):array
     {
-		Worker::log("Клиент говорит: ".$buffer);
-		return json_decode(parent::decode($buffer, $connection), true);
+		if($buffer = parent::decode($buffer, $connection))
+		{
+			Worker::log("Клиент говорит: ".$buffer);
+			return json_decode($buffer, true);
+		}
     }
 }
