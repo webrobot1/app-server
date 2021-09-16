a201970983b4900f908f41647a5ff9b0ee314c44
Стрельцов Михаил <admin@my-fantasy.ru>
2021-09-16 19:45:00 +0300

    no message

--- model/Protocols/Websocket.php
+++ model/Protocols/Websocket.php
@@ -3,14 +3,16 @@ namespace Edisom\App\server\model\Protocols;
 
 class Websocket extends \Workerman\Protocols\Websocket
 {
-    /**
-     * Decode.
-     *
-     * @param string $buffer
-     * @return string
-     */
+	
+	public static function encode($buffer, ConnectionInterface $connection)
+    {
+		Worker::log("Шлем: ".$buffer);
+		return parent::encode($buffer, $connection);
+	}
+	
     public static function decode($buffer, \Workerman\Connection\ConnectionInterface $connection):array
     {
+		Worker::log("Клиент говорит: ".$buffer);
 		return json_decode(parent::decode($buffer, $connection), true);
     }
 }
